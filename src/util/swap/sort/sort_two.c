b55531eb0cc80eb570af7fa6654a8990a393ae92
nyj001012 <nyj001012@gmail.com>
2023-02-14 15:31:17 +0900

    FEAT: added sort_two.c

--- src/util/swap/sort/sort_two.c
+++ src/util/swap/sort/sort_two.c
@@ -6,12 +6,19 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 15:18:28 by yena              #+#    #+#             */
-/*   Updated: 2023/02/14 15:18:54 by yena             ###   ########.fr       */
+/*   Updated: 2023/02/14 15:29:10 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sort_two(t_stack *a, t_stack *b)
+void	sort_two(t_stack *a)
 {
+	t_node	*head_node;
+
+	head_node = a->head_node;
+	if (head_node->value > head_node->next->value)
+		swap_a_or_b(a, SA);
+	else
+		return ;
 }
--- src/util/swap/swap_util.c
+++ src/util/swap/swap_util.c
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 15:13:39 by yena              #+#    #+#             */
-/*   Updated: 2023/02/14 15:16:24 by yena             ###   ########.fr       */
+/*   Updated: 2023/02/14 15:29:29 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	push_swap(t_stack *a, t_stack *b)
 {
 	if (a->nodes_count == 2)
-		sort_two(a, b);
+		sort_two(a);
 	else if (a->nodes_count == 3)
 		sort_three(a, b);
 	else
