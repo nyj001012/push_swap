69540806aed9f248b7d2514d8d48cb1a79db7ac5
nyj001012 <nyj001012@gmail.com>
2023-02-14 10:54:42 +0900

    FIX: changed parameter type of f from int to int *

--- src/util/stack/node/node_iter.c
+++ src/util/stack/node/node_iter.c
@@ -6,13 +6,13 @@
 /*   By: yena <yena@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 23:16:46 by yena              #+#    #+#             */
-/*   Updated: 2023/02/12 17:19:03 by yena             ###   ########.fr       */
+/*   Updated: 2023/02/14 10:29:50 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	node_iter(t_node *node, void (*f)(int))
+void	node_iter(t_node *node, void (*f)(int *))
 {
 	t_node	*temp;
 
@@ -21,7 +21,7 @@ void	node_iter(t_node *node, void (*f)(int))
 	temp = node;
 	while (temp)
 	{
-		f(temp->value);
+		f(&temp->value);
 		temp = temp->next;
 	}
 }
