a2c9a720770c49820cd626675c187ee3103461d6
nyj001012 <nyj001012@gmail.com>
2023-02-14 12:07:59 +0900

    TEST: added unit test module for reverse_rotate_a_or_b()

---
+++ test/unit/reverse_rotate_a_or_b.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate_a_or_b.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 14:42:10 by yena              #+#    #+#             */
+/*   Updated: 2023/02/14 11:58:55 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test.h"
+
+void	reverse_rotate_a_or_b_test(void)
+{
+	t_stack	a;
+	t_stack	b;
+	t_node	*a_head_node;
+	t_node	*b_head_node;
+	char	**argv;
+
+	a_head_node = NULL;
+	b_head_node = NULL;
+	argv = ft_split("0 -1 3 2 4 1", ' ');
+	init_stack(&a, &b, argv);
+	ft_printf("============ command: rra ============\n");
+	ft_printf("stack a: 1 4 2 3 -1 0 \tstack b: \n");
+	ft_printf("from reverse_rotate_a_or_b(): ");
+	reverse_rotate_a_or_b(&a, RRA);
+	ft_printf("expected => stack a: 0 1 4 2 3 -1 \tstack b: \n");
+	ft_printf("result   => stack a: ");
+	a_head_node = a.head_node;
+	while (a_head_node)
+	{
+		ft_printf("%d ", a_head_node->value);
+		a_head_node = a_head_node->next;
+	}
+	ft_printf("\tstack b: ");
+	b_head_node = b.head_node;
+	while (b_head_node)
+	{
+		ft_printf("%d ", b_head_node->value);
+		b_head_node = b_head_node->next;
+	}
+
+	ft_printf("\n\n============ command: rrb ============\n");
+	ft_printf("stack a: 0 1 4 2 3 -1 \tstack b: \n");
+	ft_printf("from reverse_rotate_a_or_b(): must do nothing\n");
+	reverse_rotate_a_or_b(&b, RRB);
+	ft_printf("expected => stack a: 0 1 4 2 3 -1 \tstack b: \n");
+	ft_printf("result   => stack a: ");
+	a_head_node = a.head_node;
+	while (a_head_node)
+	{
+		ft_printf("%d ", a_head_node->value);
+		a_head_node = a_head_node->next;
+	}
+	ft_printf("\tstack b: ");
+	b_head_node = b.head_node;
+	while (b_head_node)
+	{
+		ft_printf("%d ", b_head_node->value);
+		b_head_node = b_head_node->next;
+	}
+	destroy_stacks(&a, &b);
+	free_pointers(argv);
+}
