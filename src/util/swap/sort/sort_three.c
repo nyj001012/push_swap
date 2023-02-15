4c26ac6dfbaf2d2bae9ed991d38b3a927e49b5bd
nyj001012 <nyj001012@gmail.com>
2023-02-15 13:25:31 +0900

    FEAT: added sort for three parameters

--- src/util/swap/sort/sort_three.c
+++ src/util/swap/sort/sort_three.c
@@ -6,14 +6,37 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 15:19:02 by yena              #+#    #+#             */
-/*   Updated: 2023/02/14 15:29:57 by yena             ###   ########.fr       */
+/*   Updated: 2023/02/15 13:20:07 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sort_three(t_stack *a, t_stack *b)
+void	sort_three(t_stack *a)
 {
-	(void)a;
-	(void)b;
+	t_node	*middle;
+
+	middle = a->head_node->next;
+	if (middle == a->min_node)
+	{
+		if (middle->next == a->max_node)
+			swap_a_or_b(a, SA);
+		else
+			rotate_a_or_b(a, RA);
+	}
+	else if (middle == a->max_node)
+	{
+		if (a->head_node == a->min_node)
+			reverse_rotate_a_or_b(a, RRA);
+		else
+		{
+			reverse_rotate_a_or_b(a, RRA);
+			swap_a_or_b(a, SA);
+		}
+	}
+	else
+	{
+		swap_a_or_b(a, SA);
+		reverse_rotate_a_or_b(a, RRA);
+	}
 }
--- src/util/swap/swap_util.c
+++ src/util/swap/swap_util.c
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 15:13:39 by yena              #+#    #+#             */
-/*   Updated: 2023/02/14 15:29:29 by yena             ###   ########.fr       */
+/*   Updated: 2023/02/15 13:12:13 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	push_swap(t_stack *a, t_stack *b)
 	if (a->nodes_count == 2)
 		sort_two(a);
 	else if (a->nodes_count == 3)
-		sort_three(a, b);
+		sort_three(a);
 	else
 		sort_multiple(a, b);
 }
