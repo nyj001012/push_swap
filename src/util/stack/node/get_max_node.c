5ea9a205259ba4272d4ea05dcb2edd410d172336
nyj001012 <nyj001012@gmail.com>
2023-02-15 13:29:00 +0900

    FIX: fixed to loop until last node of stack

--- src/util/stack/node/get_max_node.c
+++ src/util/stack/node/get_max_node.c
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 16:57:22 by yena              #+#    #+#             */
-/*   Updated: 2023/02/12 17:37:08 by yena             ###   ########.fr       */
+/*   Updated: 2023/02/15 13:28:09 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_node	*get_max_node(t_stack *stack)
 
 	current = stack->head_node;
 	max_node = current;
-	while (current->next)
+	while (current)
 	{
 		if (current->value > max_node->value)
 			max_node = current;
