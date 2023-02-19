9abf04158c75f3cd17673f2d7126fdc532e75ec2
nyj001012 <nyj001012@gmail.com>
2023-02-19 16:16:33 +0900

    FIX: fixed to assign null to head_node of stack_from

--- src/util/command/push_a_or_b.c
+++ src/util/command/push_a_or_b.c
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 14:27:41 by yena              #+#    #+#             */
-/*   Updated: 2023/02/19 14:50:56 by yena             ###   ########.fr       */
+/*   Updated: 2023/02/19 16:03:45 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	push_a_or_b(t_stack *stack_from, t_stack *stack_to, int command) {
 		return;
 	temp = stack_from->head_node;
 	stack_from->head_node = stack_from->head_node->next;
+	temp->next = NULL;
 	add_head_node(&stack_to->head_node, temp);
 	stack_from->nodes_count--;
 	stack_to->nodes_count++;
