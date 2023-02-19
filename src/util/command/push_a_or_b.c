cc2c2fa884812de08138f540dbc5fe7fe3650fe9
nyj001012 <nyj001012@gmail.com>
2023-02-19 14:51:36 +0900

    FIX: fixed to add/subtract nodes_count

--- src/util/command/push_a_or_b.c
+++ src/util/command/push_a_or_b.c
@@ -6,7 +6,7 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 14:27:41 by yena              #+#    #+#             */
-/*   Updated: 2023/02/14 10:41:53 by yena             ###   ########.fr       */
+/*   Updated: 2023/02/19 14:50:56 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 void	push_a_or_b(t_stack *stack_from, t_stack *stack_to, int command) {
 	t_node *temp;
 
-	if (!stack_from->head_node)
+	if (!stack_from->nodes_count)
 		return;
 	temp = stack_from->head_node;
 	stack_from->head_node = stack_from->head_node->next;
 	add_head_node(&stack_to->head_node, temp);
+	stack_from->nodes_count--;
+	stack_to->nodes_count++;
 	if (command == PA)
 		ft_printf("pa\n");
 	else if (command == PB)
